3771c846dc138fd01e0941d35f3d8c0506d93ee6
arthurgb80 <arthurgularte80@gmail.com>
2024-02-02 15:39:35 +0300

    Update WebSecurityConfig and fix compilation issues
    - Refactor WebSecurityConfig to use SecurityFilterChain instead of WebSecurityConfigurerAdapter
    - Address deprecation warnings in WebSecurityConfig
    - Update pom.xml with necessary dependencies
    - Modify JWTFilter as needed

--- dio-spring-security-jwt/src/main/java/dio/dio/spring/security/jwt/security/WebSecurityConfig.java
+++ dio-spring-security-jwt/src/main/java/dio/dio/spring/security/jwt/security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
